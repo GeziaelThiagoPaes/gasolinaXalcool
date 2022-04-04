a8f0f0be836a8a79e0bdb4e3ea267abeae09e3ce
GeziaelThiagoPaes <geziaelthiago@hotmail.com>
2022-04-03 21:21:52 -0400

    removi a tag button do html e inseri no JS

--- js/js.js
+++ js/js.js
@@ -1,24 +1,32 @@
 
-const btn_Calcular = document.querySelector('#botao_calcular');
-btn_Calcular.addEventListener('click', function (e) {
-    e.preventDefault();
-    const valorLitro = document.querySelector('#litro').value;
-    const texto = document.querySelector('#vencedor_Comparacao');
-    const texto2 = document.querySelector('#valor_Gasolina');
-    const texto3 = document.querySelector('#valor_alcool');
-    const valorGasolina = 6.92 * valorLitro;
-    const valorAlcool = 4.93 * valorLitro;
+const texto = document.querySelector('#vencedor_Comparacao');
+const texto2 = document.querySelector('#valor_Gasolina');
+const texto3 = document.querySelector('#valor_alcool');
+const valorLitro = document.querySelector('#litro');
+
+function criarBotoes(texto, callback) {
+    const containerBotao = document.querySelector('#containerBotao');
+    const newbutton= document.createElement('button');
+    newbutton.textContent=texto;
+    callback(newbutton);
+    containerBotao.appendChild(newbutton);
+};
+criarBotoes('Calcular',(newbutton)=>{
+   newbutton.addEventListener('click', ()=>{
+    const calcularLitro = valorLitro.value;
+    const valorGasolina = 6.92 * calcularLitro;
+    const valorAlcool = 4.93 * calcularLitro;
     let valAlcool = valorAlcool.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
     let valgasolina = valorGasolina.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
-    const dividir = (valorAlcool / valorGasolina).toFixed(2);
+    const dividirValor = (valorAlcool / valorGasolina).toFixed(2);
     condicao();
     function condicao() {
-        if (dividir >= 0.7) {
+        if (dividirValor >= 0.7) {
             texto.textContent = `A gasolina é a melhor opção `;
             texto2.textContent = `valor da gasolina : ${valgasolina}`;
             texto3.textContent = `valor do álcool : ${valAlcool}`;
-        } else if(dividir < 0.7) {
-            texto.textContent = `O álcool é a melhor opção ${dividir}`;
+        } else if(dividirValor < 0.7) {
+            texto.textContent = `O álcool é a melhor opção ${dividirValor}`;
             texto2.textContent = `valor da gasolina : ${valgasolina}`;
             texto3.textContent = `valor do álcool : ${valAlcool}`;
         }else{
@@ -26,5 +34,17 @@ btn_Calcular.addEventListener('click', function (e) {
             texto2.textContent = `valor da gasolina : 0`;
             texto3.textContent = `valor do álcool : 0`;
         }
+
     }
+   });
+});
+criarBotoes('Resetar', (newbutton)=>{
+    newbutton.style.backgroundColor='blue';
+    newbutton.addEventListener('click',()=>{
+        form.reset();
+        texto.textContent='';
+        texto2.textContent='';
+        texto3.textContent='';
+    })
+    
 });
